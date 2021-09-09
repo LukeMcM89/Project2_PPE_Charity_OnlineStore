a4d1615aa574eb9dfa40269bb924c78f9e35dde5
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-09-09 13:25:03 -0400

    API key + API correctly executing

---
+++ Covid-Latest-Totals.js
@@ -0,0 +1,39 @@
+const https = require("https");
+
+// Endpoint for getting latest totals
+const endpointUrl = "https://covid-19-data.p.rapidapi.com/country/all";
+
+// RapidAPI host
+const rapidApiHost = "covid-19-data.p.rapidapi.com";
+
+// YOUR RapidAPI key
+const rapidApiKey = "a4a9dafd64mshc3dbedd703f38bbp16884djsn011747e629e7";
+
+const options = {
+  headers: {
+    "x-rapidapi-host": rapidApiHost,
+    "x-rapidapi-key": rapidApiKey,
+    useQueryString: true,
+  },
+};
+
+https
+  .get(endpointUrl, options, (res) => {
+    let body = [];
+
+    res.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    res.on("end", () => {
+      try {
+        let json = JSON.parse(body);
+        console.log(json);
+      } catch (error) {
+        console.error(error.message);
+      }
+    });
+  })
+  .on("error", (error) => {
+    console.error(error.message);
+  });
