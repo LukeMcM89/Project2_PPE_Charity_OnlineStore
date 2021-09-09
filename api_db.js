cb277811747c334bde818e5f28a25c8fdb353667
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2021-09-08 20:15:32 -0400

    https

--- api_db.js
+++ api_db.js
@@ -1,22 +1,28 @@
-const http = require(“https”);
+const http = require("https");
+
 const options = {
-	“method”: “GET”,
-	“hostname”: “covid-19-data.p.rapidapi.com”,
-	“port”: null,
-	“path”: “/country/all”,
-	“headers”: {
-		“x-rapidapi-host”: “covid-19-data.p.rapidapi.com”,
-		“x-rapidapi-key”: “a46f802688msh00a80142ba67283p1315b4jsndd2840e3e8f0”,
-		“useQueryString”: true
+	"method": "GET",
+	"hostname": "covid-19-data.p.rapidapi.com",
+	"port": null,
+	"path": "/country/all",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "aec57077b7mshe7c9917e3720dc3p12dae9jsn6c72418df371",
+		"useQueryString": true
 	}
 };
+
 const req = http.request(options, function (res) {
 	const chunks = [];
-	res.on(“data”, function (chunk) {
+
+	res.on("data", function (chunk) {
 		chunks.push(chunk);
 	});
-	res.on(“end”, function () {
+
+	res.on("end", function () {
 		const body = Buffer.concat(chunks);
 		console.log(body.toString());
 	});
 });
+
+req.end();
