0cef852aa135f67d0902413def49fafb6950df1d
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-09-11 11:28:50 -0400

    Seeds initials for products

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -4,13 +4,23 @@ CREATE DATABASE ppestore_db;
 
 CREATE TABLE shopper (
     shopper_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    shoppername VARCHAR(25) NOT NULL,
+    shopper_name VARCHAR(25) NOT NULL,
     shopper_password VARCHAR(15) NOT NULL,
     date_created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE TABLE products (
+    products_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    products_name VARCHAR(25) NOT NULL,
+    products_description TEXT(40),
+   --COME BACK TO PROD IMAGES
+    CONSTRAINT products_id FOREIGN KEY 
+);
+
 CREATE TABLE inventory (
-    inventory_id INT NOT NULL AUTO_INCREMEBNT PRIMARY KEY,
-    inventory_available SMALLINT(6) NOT NULL DEFAULT 0
-    CONSTRAINT product_id FOREIGN KEY
-)
+    inventory_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    inventory_available SMALLINT(6) NOT NULL DEFAULT 0,
+    CONSTRAINT product_id FOREIGN KEY 
+);
+
+--PRODUCTS - MASKS, HANDSANITIZER BOTTLE, FACE SHIELD, GLOVES, HAND SANTIZER WIPES, TOILET PAPER
