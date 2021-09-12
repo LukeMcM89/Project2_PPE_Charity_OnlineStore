8ca3e8088cfec203638d89b371a21457e256dfa3
Alex Palmer <alexpalmer37@gmail.com>
2021-09-12 14:55:42 -0400

    login, initials / layout

--- develop/models/Shopper.js
+++ develop/models/Shopper.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 class Shopper extends Model {
     checkPassword(loginPw) {
-        //COME BACK TO THIS 
+        return bcrypt.compareSync(loginPw, this.password);
     }
 };
 
@@ -20,6 +20,14 @@ Shopper.init(
             type: DataTypes.STRING,
             allowNull: false,
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+        },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -29,6 +37,12 @@ Shopper.init(
         },
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
